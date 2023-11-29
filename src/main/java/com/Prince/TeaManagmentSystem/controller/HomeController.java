3e80fbac21b5174f43b04d7cff1f47c5d124c2d5
prince-menaria <princemenaria04@gmail.com>
2023-11-29 16:45:22 +0530

    all file update

--- src/main/java/com/Prince/TeaManagmentSystem/controller/HomeController.java
+++ src/main/java/com/Prince/TeaManagmentSystem/controller/HomeController.java
@@ -42,8 +42,6 @@ public class HomeController {
 		System.err.println("vendor name::" + vendorName);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Disposition", "inline; filename=example.pdf");
-		
-		System.err.println();
 
 		return null;
 	}
--- src/main/java/com/Prince/TeaManagmentSystem/repository/TeaRepository.java
+++ src/main/java/com/Prince/TeaManagmentSystem/repository/TeaRepository.java
@@ -2,7 +2,6 @@ package com.Prince.TeaManagmentSystem.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,17 +10,29 @@ import com.Prince.TeaManagmentSystem.entity.Tea;
 
 @Repository
 public interface TeaRepository extends JpaRepository<Tea, Long> {
-
-	@Query(value = "SELECT * FROM TeaInventoryManagment.teas WHERE create_date BETWEEN :startDate AND :endDate ", nativeQuery = true)
+	
+	@Query(value = "SELECT * FROM teas WHERE create_date BETWEEN :startDate AND :endDate ", nativeQuery = true)
 	List<Tea> findByDate(String startDate, String endDate);
 
-	@Query(value = "SELECT * FROM TeaInventoryManagment.teas WHERE vendor_id = :vendorId ", nativeQuery = true)
+	@Query(value = "SELECT * FROM teas WHERE vendor_id = :vendorId ", nativeQuery = true)
 	List<Tea> findByVendorId(Long vendorId);
 
-	@Query(value = "SELECT * FROM TeaInventoryManagment.teas WHERE vendor_id = :vendorId AND create_date BETWEEN :startDate AND :endDate ", nativeQuery = true)
+	@Query(value = "SELECT * FROM teas WHERE vendor_id = :vendorId AND create_date BETWEEN :startDate AND :endDate ", nativeQuery = true)
 	List<Tea> findByVendorIdStartDateEndDate(Long vendorId, String startDate, String endDate);
 
-	@Query(value = "SELECT * FROM TeaInventoryManagment.teas f order by f.t_id desc ", nativeQuery = true)
+	@Query(value = "SELECT * FROM teas f order by f.t_id desc ", nativeQuery = true)
 	List<Tea> findAllOrderedByIdDesc();
+
+//	@Query(value = "SELECT * FROM TeaInventoryManagment.teas WHERE create_date BETWEEN :startDate AND :endDate ", nativeQuery = true)
+//	List<Tea> findByDate(String startDate, String endDate);
+//
+//	@Query(value = "SELECT * FROM TeaInventoryManagment.teas WHERE vendor_id = :vendorId ", nativeQuery = true)
+//	List<Tea> findByVendorId(Long vendorId);
+//
+//	@Query(value = "SELECT * FROM TeaInventoryManagment.teas WHERE vendor_id = :vendorId AND create_date BETWEEN :startDate AND :endDate ", nativeQuery = true)
+//	List<Tea> findByVendorIdStartDateEndDate(Long vendorId, String startDate, String endDate);
+//
+//	@Query(value = "SELECT * FROM TeaInventoryManagment.teas f order by f.t_id desc ", nativeQuery = true)
+//	List<Tea> findAllOrderedByIdDesc();
 
 }
